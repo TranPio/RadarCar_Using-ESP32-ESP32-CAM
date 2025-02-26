ef6e9d72135e4ad0981dbbdf80177342823ab904
Trần Hoài Phú <phubao789@gmail.com>
2025-02-26 22:36:28 +0700

    Thay doi code chua du lieu ca nhan

--- ESP32_1_2/ESP32_1_2.ino
+++ ESP32_1_2/ESP32_1_2.ino
@@ -6,16 +6,16 @@
 #include <addons/RTDBHelper.h>
 
 // 1. Thông tin WiFi
-#define WIFI_SSID "Nhom11_NT131.P12"
-#define WIFI_PASSWORD "12345678"
+#define WIFI_SSID "---------"
+#define WIFI_PASSWORD "--------"
 
 // 2. Thông tin API Key và URL
-#define API_KEY "AIzaSyD3NsDrxPrOLWapTfT7COBwgWp-LntOrtE"
-#define DATABASE_URL "https://esp32-3502a-default-rtdb.asia-southeast1.firebasedatabase.app/"
+#define API_KEY "@Your_API"
+#define DATABASE_URL "@Your_URL_Database"
 
 // 3. Thông tin tài khoản Firebase
-#define USER_EMAIL "22521106@gm.uit.edu.vn"
-#define USER_PASSWORD "Abc123"
+#define USER_EMAIL "@Your Email Firebase"
+#define USER_PASSWORD "@Your Password"
 
 // Các chân kết nối
 #define TRIG_PIN  23  // ESP32 pin GPIO23 connected to Ultrasonic Sensor's TRIG pin
